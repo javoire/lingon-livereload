6aa5468adb1d10d23c68868463e57960771aab54
Jonatan Dahl <jo_da12@hotmail.com>
2014-03-06 00:36:13 +0100

    gulp tweaks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,43 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    mocha = require('gulp-mocha'),
+    notify = require('gulp-notify');
+
+var watching = false;
+
+var mochaFail = function() {
+  if (watching) { // don't kill gulp if watching...
+    this.emit('end');
+  }
+}
+
+var jshintFail = function() {
+  if (watching) { // don't kill gulp if watching...
+    this.emit('end');
+  }
+}
 
 gulp.task('jshint', function() {
   return gulp.src('./lib/*')
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail')
+      .on('error', jshintFail)
+    )
+    .on('error', notify.onError('<%= error.message %>'));
+})
+
+gulp.task('mocha', function() {
+  return gulp.src('./test/*')
+    .pipe(mocha({reporter: 'spec'})
+      .on('error', mochaFail)
+    )
+    .on('error', notify.onError('<%= error.message %>'));
 })
 
 gulp.task('watch', function() {
-  gulp.watch('./lib/*', ['jshint']);
+  watching = true; // CI fix
+  gulp.watch('./lib/*', ['jshint', 'mocha']);
 })
 
-gulp.task('default', ['jshint', 'watch']);
+gulp.task('default', ['jshint', 'mocha', 'watch']);
