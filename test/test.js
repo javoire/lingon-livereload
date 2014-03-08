c7bf3331abd2a59e77e2d4933820bc13f408d50c
Jonatan Dahl <jo_da12@hotmail.com>
2014-03-08 12:57:53 +0100

    livereload inject script test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
 var oj = require('orangejuice'),
     livereload = require('../lib/orangejuice-livereload');
 
+// retarded test
 describe('orangejuice-livereload', function() {
   it('should init', function() {
     livereload(oj);
